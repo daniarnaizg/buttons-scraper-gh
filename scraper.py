e597446624a8c2e846181457d3342433528ff0bb
daniarnaizg <daniarnaizg@gmail.com>
2023-02-09 19:06:23 +0100

    Test json reformat

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import time
 
 BASE_URL = "https://www.myinstants.com/es/index/es/"
 
@@ -28,7 +29,7 @@ def get_instants(content):
     for item in instants:
         sound_title = str(item['title'].split(
             'Reproduce el sonido de ')[1].strip())
-        media_url = str(item['onclick'].split('\'')[1].strip())
+        media_url = str(item['onclick']).split('\'')[1].strip()
         url_list.append(
             {'url': f'https://www.myinstants.com{media_url}', 'title': sound_title})
     return url_list
@@ -38,18 +39,21 @@ def get_instants(content):
 if __name__ == '__main__':
     pages = get_pages()
 
-    full_list = []
     final_dict = {}
+    final_dict["timestamp"] = str(time.time()).split('.')[0]
 
+    full_list = []
     for page in pages:
         print(f'[INFO] Getting page: {page}')
         content = requests.get(page, headers=HEADERS).content
         instants = get_instants(content)
-        full_list.extend(instants)
-    final_dict['instants'] = full_list
+        full_list = full_list + instants
+    final_dict["data"] = full_list
 
     print(f'[INFO] {len(full_list)} total found')
 
+    print(final_dict)
+
     # save to json file
-    with open('./data/instants.json', 'w') as f:
+    with open('./data/instants.json', 'w', encoding='UTF-8') as f:
         json.dump(final_dict, f)
