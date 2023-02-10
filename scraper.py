0dc29c066ba85c21babfe727af7c34e6ad569393
daniarnaizg <daniarnaizg@gmail.com>
2023-02-10 12:55:28 +0100

    Scrape global and trend, english and spanish

--- scraper.py
+++ scraper.py
@@ -3,7 +3,11 @@ import requests
 import json
 import time
 
-BASE_URL = "https://www.myinstants.com/es/index/es/"
+BASE_URL = "https://www.myinstants.com"
+GLOBAL_EN = "/en/best_of_all_time/us/"
+TRENDING_EN = "/en/index/us/"
+GLOBAL_ES = "/es/best_of_all_time/es/"
+TRENDING_ES = "/es/index/es/"
 
 HEADERS = {
     'Access-Control-Allow-Origin': '*',
@@ -14,46 +18,64 @@ HEADERS = {
 }
 
 
-def get_pages(n_pages=3):
+def get_pages(n_pages=3, url=BASE_URL):
     pages = []
     for i in range(1, n_pages + 1):
-        pages.append(BASE_URL + f'?page={str(i)}')
+        pages.append(url + f'?page={str(i)}')
     return pages
 
 
-def get_instants(content):
+def get_instants(content, lang='en'):
     url_list = []
     soup = BeautifulSoup(content, 'html.parser')
     instants = soup.find_all(class_="small-button")
     print(f'[INFO] {len(instants)} found')
-    for item in instants:
-        sound_title = str(item['title'].split(
-            'Reproduce el sonido de ')[1].strip())
+    for item in instants: 
+        if lang == 'en':
+            # "Play XXXXXX sound"
+            sound_title = str(item['title'].split(
+                'Play ')[1].split(' sound')[0])
+        elif lang == 'es':
+            # "Reproduce el sonido de XXXXXX"
+            sound_title = str(item['title'].split(
+                'Reproduce el sonido de ')[1].strip())
         media_url = str(item['onclick']).split('\'')[1].strip()
         url_list.append(
             {'url': f'https://www.myinstants.com{media_url}', 'title': sound_title})
     return url_list
 
 
+def get_content(category, lang='en'):
+    pages = get_pages(url=BASE_URL + category)
+    current_list = []
+    for page in pages:
+        content = requests.get(page, headers=HEADERS).content
+        instants = get_instants(content, lang=lang)
+        current_list = current_list + instants
+    print(f'[INFO] Total {category}: {len(current_list)}')
+    return current_list
+
+
 # main
 if __name__ == '__main__':
-    pages = get_pages()
 
     final_dict = {}
+
+    print(f'[INFO] Timestamp: {str(time.time()).split(".")[0]}')
     final_dict["timestamp"] = str(time.time()).split('.')[0]
 
-    full_list = []
-    for page in pages:
-        print(f'[INFO] Getting page: {page}')
-        content = requests.get(page, headers=HEADERS).content
-        instants = get_instants(content)
-        full_list = full_list + instants
-    final_dict["data"] = full_list
+    print('[INFO] Getting global english')
+    final_dict["global_en"] = get_content(GLOBAL_EN, lang='en')
 
-    print(f'[INFO] {len(full_list)} total found')
+    print('[INFO] Getting trending english')
+    final_dict["trending_en"] = get_content(TRENDING_EN, lang='en')
 
-    # print(final_dict)
+    print('[INFO] Getting global spanish')
+    final_dict["global_es"] = get_content(GLOBAL_ES, lang='es')
+
+    print('[INFO] Getting trending spanish')
+    final_dict["trending_es"] = get_content(TRENDING_ES, lang='es')
 
     # save to json file
-    with open('./data/instants.json', 'w') as f:
+    with open('./data/instants.json', 'w', encoding='UTF-8') as f:
         json.dump(final_dict, f)
